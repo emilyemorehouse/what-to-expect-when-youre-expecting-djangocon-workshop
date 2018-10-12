e1328ac33611008046c98a7c8c017325da427438
Emily Morehouse <emilyemorehouse@gmail.com>
2018-10-12 14:26:23 -0600

    ☔️ (Django) 1 - Add our first test!

---
+++ regression-testing-in-django/code/src/regression_testing_boilerplate/tests.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+from django.test import TestCase
+from django.urls import reverse
+
+
+class PageOpenTestCase(TestCase):
+    def test_home_page_exists(self):
+        url = reverse("home")
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
+
+    def test_about_page_exists(self):
+        url = reverse("about")
+        r = self.client.get(url)
+        self.assertEqual(r.status_code, 200)
