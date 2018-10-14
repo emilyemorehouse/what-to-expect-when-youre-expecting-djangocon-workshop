6a345708c8637d111f2eba0ad299d2508721c375
Emily Morehouse <emilyemorehouse@gmail.com>
2018-10-14 08:11:46 -0700

    ☔️ (Django) 4b Add form tests for Profile

--- regression-testing-in-django/code/src/profiles/tests.py
+++ regression-testing-in-django/code/src/profiles/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from .models import BaseProfile
-from .forms import UserForm
+from .forms import UserForm, ProfileForm
 
 User = get_user_model()
 
@@ -30,3 +30,23 @@ class UserTestCase(TestCase):
         data = {"name": u.name}
         form = UserForm(data=data)
         self.assertTrue(form.is_valid())
+
+    def test_valid_profile_form_1(self):
+        data = {"bio": None}
+        form = ProfileForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_valid_profile_form_2(self):
+        data = {"bio": ""}
+        form = ProfileForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_valid_profile_form_3(self):
+        data = {"bio": "e" * 200}
+        form = ProfileForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_profile_form(self):
+        data = {"bio": "e" * 201}
+        form = ProfileForm(data=data)
+        self.assertFalse(form.is_valid())
