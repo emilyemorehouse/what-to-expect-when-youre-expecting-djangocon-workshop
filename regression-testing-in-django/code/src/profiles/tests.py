5b2f227d88540bf51532ec4d98356f17dacdb3be
Emily Morehouse <emilyemorehouse@gmail.com>
2018-10-13 23:01:38 -0700

    ☔️ (Django) 2 - Add User and BaseProfile basic test

--- regression-testing-in-django/code/src/profiles/tests.py
+++ regression-testing-in-django/code/src/profiles/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import BaseProfile
 
-# Create your tests here.
+User = get_user_model()
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        u = User.objects.get_or_create(email="solo@example.com")
+
+    def test_base_profile_created(self):
+        u = User.objects.get(email="solo@example.com")
+
+        self.assertIsNotNone(u.id)
+        self.assertEqual(u.email, "solo@example.com")
+        self.assertIsNotNone(u.profile)
+        self.assertIsNotNone(u.profile.slug)
